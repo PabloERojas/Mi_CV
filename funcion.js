6962f9675d806403ce44dfc183b5dad042e58da1
Pablo <126348051+PabloERojas@users.noreply.github.com>
2023-02-27 20:50:06 -0300

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,5 @@
+alert("Hola, Bienvenido a mí Hoja de Vida. Espero que podamos ponernos en contacto pronto y así poder conocernos. Que estés bien y tengas una lectura amena.");
+
+document.getElementById('boton_color').addEventListener('click',function() {
+   document.body.style.backgroundColor = '#fff';
+})
